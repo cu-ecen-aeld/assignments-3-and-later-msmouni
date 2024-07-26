250f98406b32506571a4e13ba0d64bfece5c7722
msmouni <marwan-smouni@hotmail.fr>
2024-07-26 20:30:46 +0200

    Impl System calls: `system()` + `exec()` + child process + stdout redirection

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -6,94 +6,200 @@
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
+ */
 bool do_system(const char *cmd)
 {
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+    /*
+     * TODO  add your code here
+     *  Call the system() function with the command set in the cmd
+     *   and return a boolean true if the system() call completed with success
+     *   or false() if it returned a failure
+     */
+    return system(cmd) == 0;
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables are command to execute.
+ *   followed by arguments to pass to the command
+ *   Since exec() does not perform path expansion, the command to execute needs
+ *   to be an absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in execv()
+ * @return true if the command @param ... with arguments @param arguments were executed successfully
+ *   using the execv() call, false if an error occurred, either in invocation of the
+ *   fork, waitpid, or execv() command, or if a non-zero return value was returned
+ *   by the command issued in @param arguments with the specified arguments.
+ */
 
 bool do_exec(int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    /*
+     * TODO:
+     *   Execute a system command by calling fork, execv(),
+     *   and wait instead of system (see LSP page 161).
+     *   Use the command[0] as the full path to the command to execute
+     *   (first argument to execv), and use the remaining arguments
+     *   as second argument to the execv() command.
+     *
+     */
 
     va_end(args);
+
+    int ch_pid = fork();
+
+    if (ch_pid < 0)
+    {
+        return false;
+    }
+    else if (ch_pid == 0)
+    {
+        // Child process
+        execv(command[0], command);
+
+        printf("This should not be printed\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        // Parent process
+        // wait(&status): Suspends execution of the calling process until one of its children terminates.
+        // waitpid(pid, &status, options): waits for a specific child process to change state.
+        int status;
+        pid_t result = waitpid(ch_pid, &status, 0);
+
+        if (result == -1)
+        {
+            // If waitpid() returns -1, an error occurred
+            return false;
+        }
+
+        if (WIFEXITED(status))
+        {
+            printf("Child exited with status = %d\n", WEXITSTATUS(status));
+
+            if (WEXITSTATUS(status) != 0)
+            {
+                return false;
+            }
+        }
+        else if (WIFSIGNALED(status))
+        {
+            printf("Child terminated by signal %d\n", WTERMSIG(status));
+        }
+        else
+        {
+            printf("Child terminated abnormally\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 
     return true;
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    /*
+     * TODO
+     *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+     *   redirect standard out to a file specified by outputfile.
+     *   The rest of the behaviour is same as do_exec()
+     *
+     */
 
     va_end(args);
+
+    int ch_pid = fork();
+
+    if (ch_pid < 0)
+    {
+        return false;
+    }
+    else if (ch_pid == 0)
+    {
+        // Child process
+
+        int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+        if (fd < 0)
+        {
+            printf("Error opening %s", outputfile);
+            exit(EXIT_FAILURE);
+        }
+
+        if (dup2(fd, STDOUT_FILENO) < 0)
+        {
+            perror("dup2");
+            exit(EXIT_FAILURE);
+        }
+
+        close(fd);
+
+        execv(command[0], command);
+
+        printf("This should not be printed\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        // Parent process
+        // wait(&status): Suspends execution of the calling process until one of its children terminates.
+        // waitpid(pid, &status, options): waits for a specific child process to change state.
+        int status;
+        pid_t result = waitpid(ch_pid, &status, 0);
+
+        if (result == -1)
+        {
+            // If waitpid() returns -1, an error occurred
+            return false;
+        }
+
+        if (WIFEXITED(status))
+        {
+            printf("Child exited with status = %d\n", WEXITSTATUS(status));
+
+            if (WEXITSTATUS(status) != 0)
+            {
+                return false;
+            }
+        }
+        else if (WIFSIGNALED(status))
+        {
+            printf("Child terminated by signal %d\n", WTERMSIG(status));
+        }
+        else
+        {
+            printf("Child terminated abnormally\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 
     return true;
 }
